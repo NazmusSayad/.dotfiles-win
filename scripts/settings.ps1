e1ab90bff8f2ce2874f81263d75388d25eecb891
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-08-23 09:21:08 +0600

    M scripts/settings-user.ps1  M scripts/settings.ps1

--- scripts/settings.ps1
+++ scripts/settings.ps1
@@ -9,6 +9,3 @@ Remove-Item "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Progr
 Remove-Item "C:\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\OneDrive.exe" -ErrorAction Continue
 Remove-Item "C:\Windows\System32\OneDriveSetup.exe" -ErrorAction Continue
 Remove-Item "C:\Windows\SysWOW64\OneDriveSetup.exe" -ErrorAction Continue
-
-New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -Force
-New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InProcServer32" -Force
